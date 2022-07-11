1452fc37fb6846bb6353fcc4ae2adc6514754c8e
Shahmir <sham9871@gmail.com>
2022-07-11 22:59:15 +0800

    added css and js addons

--- content.js
+++ content.js
@@ -14,5 +14,28 @@ try {
     // )
 
 } catch(error) {
-    console.log("Error", error)
+    console.log("Errorrrrr", error)
 }
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function changeHeading() {
+    await sleep(5000)
+    console.log("changing heading!")
+    let mainHeading = document.getElementById("main-heading")
+    
+    const logo = document.createElement("img")
+    const div = document.createElement("div")
+    div.style.width = "100%"
+    // logo.setAttribute("src", "https://i.ibb.co/jvGb7fM/tigerlogo.png")
+    logo.setAttribute("src", "https://i.postimg.cc/Fs3ktsTr/tigerlogo.png")
+    logo.setAttribute("width", "50px")
+    logo.setAttribute("height", "50px")
+    div.appendChild(logo)
+
+    mainHeading.parentNode.replaceChild(div, mainHeading)
+}
+
+changeHeading()
