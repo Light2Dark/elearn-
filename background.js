62d0936b7818d4934ff414c6b154334f8da15dbd
Shahmir <sham9871@gmail.com>
2022-09-06 23:35:26 +0800

    added download options

--- background.js
+++ background.js
@@ -3,3 +3,12 @@ chrome.runtime.onInstalled.addListener(() => {
         console.log("current theme is", theme)
     });
 });
+
+// keep's browser alive every 2 mins
+let tab = chrome.tabs.query({url: "https://elearn.sunway.edu.my/*"})
+console.log(tab)
+function keepMeAlive() {
+    setInterval(() => {
+        console.log("keeep meeee allivee")
+    }, 120000)
+}
--- popup/popup.js
+++ popup/popup.js
@@ -8,3 +8,21 @@ selectMenu.addEventListener("change", async () => {
     let [tab] = await chrome.tabs.query({url: "https://elearn.sunway.edu.my/*"})
     chrome.tabs.reload(tabID=tab.id)
 })
+
+let downloadButton = document.getElementById("downloadButton")
+
+// When the button is clicked, download all links in the current tab
+downloadButton.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: downloadAllFiles,
+    });
+  });
+  
+//   The body of this function will be executed as a content script inside the
+//   current page
+  function downloadAllFiles() {
+    alert("hello")
+  }
